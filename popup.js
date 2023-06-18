48441f934f9fab9f621d9139db15e411da2ef9c3
aismlv <adilzhan.ismailov@gmail.com>
2023-06-18 11:49:52 +0100

    Add button click feedback, update HTML

--- popup.js
+++ popup.js
@@ -1,3 +1,5 @@
+const SEPARATOR = '\n';
+
 document.addEventListener('DOMContentLoaded', function () {
     loadBlocklist();
     document.getElementById('save').addEventListener('click', saveBlocklist);
@@ -6,15 +8,17 @@ document.addEventListener('DOMContentLoaded', function () {
 function loadBlocklist() {
     chrome.storage.sync.get('blocklist', function (data) {
         if (data.blocklist) {
-            document.getElementById('blocklist').value = data.blocklist.join(', ');
+            document.getElementById('blocklist').value = data.blocklist.join(SEPARATOR);
         }
     });
 }
 
 function saveBlocklist() {
     console.log('Save button has been pressed'); // log button press
-    const blocklist = document.getElementById('blocklist').value.split(',').map(s => s.trim());
+    const blocklist = document.getElementById('blocklist').value.split(SEPARATOR).map(s => s.trim()).filter(Boolean);
     chrome.storage.sync.set({ blocklist: blocklist }, function () {
         console.log('Blocklist is set to ', blocklist);
+        document.getElementById('status').innerText = 'Updated. Reload tha page or scroll down to load new videos to take effect';
+        setTimeout(function () { document.getElementById('status').innerText = ''; }, 3000);
     });
 }
